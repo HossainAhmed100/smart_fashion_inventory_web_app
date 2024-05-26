0a95dc093277a36dea9d725f15916802c52be146
HossainAhmed100 <successfulllife101@gmail.com>
2024-05-26 13:21:33 +0600

    Tag Added Functionality Added and Also Popup Notification Added

--- src/Pages/TagList/TagList.jsx
+++ src/Pages/TagList/TagList.jsx
@@ -2,6 +2,8 @@ import {Card, CardBody, Input, Button, Chip } from "@nextui-org/react";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { IoMdPricetag, IoIosAddCircle } from "react-icons/io";
+import { toast } from 'react-hot-toast';
+
 
 function TagList() {
     const initialNumber = [34, 53, 42];
@@ -9,19 +11,26 @@ function TagList() {
     const [cuttingNo, setCuttingNo] = useState(initialNumber);
     const [styleName, setStyleName] = useState(initialStyleName);
     const { register, handleSubmit, reset, formState: { errors }} = useForm();
+    const { register: styelRegister, reset: styleInputReset, handleSubmit: styleHandleSubmit, formState: { errors: styleErrors } } = useForm();
     const onSubmit = (data) => {
+        toast.success('Added Successfully!',{position: 'bottom-right', duration: 2000})
         setCuttingNo([...cuttingNo, data.cuttingNo]);
         reset()
     };
+    const onStyleSubmit = (data) => {
+        toast.success('Added Successfully!',{position: 'bottom-right', duration: 2000})
+        setStyleName([...styleName, data.styleName]);
+        styleInputReset()
+    };
 
     const deleteStyleName = (itemToRemove) => {
-        setStyleName(styleName.filter(item => item !== itemToRemove));
+      setStyleName(styleName.filter(item => item !== itemToRemove));
       if (styleName.length === 1) {
         setStyleName(initialStyleName);
       }
     };
     const deleteCuttingNo = (itemToRemove) => {
-        setCuttingNo(cuttingNo.filter(item => item !== itemToRemove));
+      setCuttingNo(cuttingNo.filter(item => item !== itemToRemove));
       if (cuttingNo.length === 1) {
         setCuttingNo(initialNumber);
       }
@@ -32,16 +41,16 @@ function TagList() {
         <Card>
           <CardBody className="p-6 h-72 flex flex-col gap-6">
             <div>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <h1 className="mb-2">Cutting Number</h1>
+            <form onSubmit={styleHandleSubmit(onStyleSubmit)}>
+                <h1 className="mb-2">Add Style Name</h1>
                 <div className="flex  justify-center gap-3">
                 <Input
                 radius="sm"
                 type="text"
                 variant="bordered"
                 placeholder="Example.. Polo Shirt, J.P. Tor"
-                {...register("styleName", { required: true })}
-                isInvalid={errors.styleName ? true : false}
+                {...styelRegister("styleName", { required: true })}
+                isInvalid={styleErrors.styleName ? true : false}
                 errorMessage="Please Type Stayle Name"
                 className="flex-1"
                 />
@@ -51,12 +60,14 @@ function TagList() {
                 </div>
             </form>
             </div>
-            <div className="p-2 border flex gap-2 border-gray-600 rounded-md w-full flex-1">
+            <div className="p-2 border border-gray-600 rounded-md w-full h-full">
+            <div className="flex flex-wrap gap-2">
             {styleName.map((name, index) => (
                <Chip key={index} onClose={() => deleteStyleName(name)} variant="flat">
                 {name}
                </Chip>
             ))}
+            </div>
             </div>
           </CardBody>
         </Card>
@@ -64,7 +75,7 @@ function TagList() {
           <CardBody className="p-6 h-72 flex flex-col gap-6">
             <div>
             <form onSubmit={handleSubmit(onSubmit)}>
-                <h1 className="mb-2">Cutting Number</h1>
+                <h1 className="mb-2">Add Cutting Number</h1>
                 <div className="flex  justify-center gap-3">
                 <Input
                 radius="sm"
@@ -82,12 +93,14 @@ function TagList() {
                 </div>
             </form>
             </div>
-            <div className="p-2 border flex gap-2 border-gray-600 rounded-md w-full flex-1">
+            <div className="p-2 border border-gray-600 rounded-md w-full h-full">
+            <div className="flex flex-wrap gap-2">
             {cuttingNo.map((fruit, index) => (
                 <Chip key={index} onClose={() => deleteCuttingNo(fruit)} variant="flat">
                 {fruit}
                 </Chip>
             ))}
+            </div>
             </div>
           </CardBody>
         </Card>
--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { RouterProvider} from "react-router-dom";
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { router } from './Routes/Routes';
 import './index.css'
-
+import { Toaster } from 'react-hot-toast';
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -14,6 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <HelmetProvider >
           <div className="dark text-foreground bg-background">
             <RouterProvider router={router} />
+            <Toaster />
           </div>
         </HelmetProvider>
       </QueryClientProvider>
