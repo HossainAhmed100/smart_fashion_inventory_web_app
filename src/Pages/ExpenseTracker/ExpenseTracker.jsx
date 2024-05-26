e3c235681eec644da418fdd08c9b4f5c3a257218
HossainAhmed100 <successfulllife101@gmail.com>
2024-05-26 15:10:29 +0600

    Expense Page UI And Functionality Added

--- src/Pages/ExpenseTracker/ExpenseTracker.jsx
+++ src/Pages/ExpenseTracker/ExpenseTracker.jsx
@@ -1,10 +1,353 @@
+import React from "react";
+import {
+  Table,
+  TableHeader,
+  TableColumn,
+  TableBody,
+  TableRow,
+  TableCell,
+  Input,
+  Button,
+  DropdownTrigger,
+  Dropdown,
+  DropdownMenu,
+  DropdownItem,
+  Chip,
+  Pagination,
+} from "@nextui-org/react";
+import {VerticalDotsIcon} from "./VerticalDotsIcon";
+import {SearchIcon} from "./SearchIcon";
+import {ChevronDownIcon} from "./ChevronDownIcon";
+import {capitalize} from "./utils";
+import { useQuery } from "@tanstack/react-query";
+import useAxiosPublic from "../../hooks/useAxiosPublic";
+import AddNewDataBtn from "../../components/Button/AddNewDataBtn";
 
-function ExpenseTracker() {
+const statusColorMap = {paid: "success", due: "danger"};
+
+const columns = [
+  {name: "EXPENSE ID", uid: "itemNo"},
+  {name: "EXPENSE DATE", uid: "expense_date"},
+  {name: "Factory NAME", uid: "expense_factoryName"},
+  {name: "EXPENSE DESCRIPTION", uid: "expense_desceiption"},
+  {name: "PAYMENT STATUS", uid: "expense_status"},
+  {name: "EXPENSE AMOUNT", uid: "expense_amount", sortable: true},
+  {name: "ACTIONS", uid: "actions"},
+];
+
+const statusOptions = [{name: "Expense Amount Paid", uid: "paid"},{name: "Expense Amount Due", uid: "due"}];
+
+const INITIAL_VISIBLE_COLUMNS = ["itemNo", "expense_date", "expense_factoryName", "expense_desceiption", "expense_amount", "expense_status", "actions"];
+
+const reportData = [
+    {
+       "_id": "feo8698608470h8394", 
+       "itemNo": "878972", 
+       "expense_date": "2024-05-16", 
+       "expense_factoryName": "Embrodery", 
+       "expense_desceiption": "Niddle 1,000 Pcs", 
+       "expense_amount": "2,340", 
+       "expense_status": "paid",
+    },
+    {
+       "_id": "feo8698808470h8394", 
+       "itemNo": "878072", 
+       "expense_date": "2024-05-26", 
+       "expense_factoryName": "Printing", 
+       "expense_desceiption": "Stauff Coffe 20 Cup", 
+       "expense_amount": "2,340", 
+       "expense_status": "due",
+    }
+];
+
+export default function ExpenseTracker() {
+  const [filterValue, setFilterValue] = React.useState("");
+  const [selectedKeys, setSelectedKeys] = React.useState(new Set([]));
+  const [visibleColumns, setVisibleColumns] = React.useState(new Set(INITIAL_VISIBLE_COLUMNS));
+  const [statusFilter, setStatusFilter] = React.useState("all");
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+//   const axiosPublic = useAxiosPublic();
+  const [sortDescriptor, setSortDescriptor] = React.useState({column: "expense_amount",direction: "ascending",});
+  const [page, setPage] = React.useState(1);
+
+  const hasSearchFilter = Boolean(filterValue);
+
+//   const {data: reportData = [], isLoading: isReportLoading} = useQuery({
+//     queryKey: ["reportData"],
+//     queryFn: async()=>{
+//       const res = await axiosPublic.get("/reportData");
+//       return res.data;
+//     }
+//   })
+
+  const headerColumns = React.useMemo(() => {
+    if (visibleColumns === "all") return columns;
+
+    return columns.filter((column) => Array.from(visibleColumns).includes(column.uid));
+  }, [visibleColumns]);
+
+  const filteredItems = React.useMemo(() => {
+    let filteredUsers = [...reportData];
+
+    if (hasSearchFilter) {
+      filteredUsers = filteredUsers.filter((data) =>
+        data.styleName.toLowerCase().includes(filterValue.toLowerCase()),
+      );
+    }
+    if (statusFilter !== "all" && Array.from(statusFilter).length !== statusOptions.length) {
+      filteredUsers = filteredUsers.filter((data) =>
+        Array.from(statusFilter).includes(data.expense_status),
+      );
+    }
+
+    return filteredUsers;
+  }, [hasSearchFilter, filterValue, statusFilter]);
+
+  const pages = Math.ceil(filteredItems.length / rowsPerPage);
+
+  const items = React.useMemo(() => {
+    const start = (page - 1) * rowsPerPage;
+    const end = start + rowsPerPage;
+
+    return filteredItems.slice(start, end);
+  }, [page, filteredItems, rowsPerPage]);
+
+  const sortedItems = React.useMemo(() => {
+    return [...items].sort((a, b) => {
+      const first = a[sortDescriptor.column];
+      const second = b[sortDescriptor.column];
+      const cmp = first < second ? -1 : first > second ? 1 : 0;
+
+      return sortDescriptor.direction === "descending" ? -cmp : cmp;
+    });
+  }, [sortDescriptor, items]);
+
+  const renderCell = React.useCallback((item, columnKey) => {
+    const cellValue = item[columnKey];
+
+    switch (columnKey) {
+      case "expense_date":
+        return (
+          <div className="flex flex-col">
+            <p className="text-bold text-small capitalize">{cellValue}</p>
+          </div>
+        );
+      case "expense_desceiption":
+        return (
+          <div className="flex flex-col">
+            <p className="text-bold text-small capitalize">{cellValue} Stitch</p>
+          </div>
+        );
+      case "expense_amount":
+        return (
+          <div className="flex flex-col">
+            <p className="text-bold text-small capitalize">Tk. {cellValue}</p>
+          </div>
+        );
+      case "expense_status":
+        return (
+          <Chip className="capitalize" color={statusColorMap[item.expense_status]} size="sm" variant="flat">
+            {cellValue}
+          </Chip>
+        );
+      case "actions":
+        return (
+          <div className="relative flex justify-end items-center gap-2">
+            <Dropdown>
+              <DropdownTrigger>
+                <Button isIconOnly size="sm" variant="light">
+                  <VerticalDotsIcon className="text-default-300" />
+                </Button>
+              </DropdownTrigger>
+              <DropdownMenu>
+                <DropdownItem aria-label="view">View</DropdownItem>
+                <DropdownItem aria-label="edit">Edit</DropdownItem>
+                <DropdownItem aria-label="delete">Delete</DropdownItem>
+              </DropdownMenu>
+            </Dropdown>
+          </div>
+        );
+      default:
+        return cellValue;
+    }
+  }, []);
+
+  const onNextPage = React.useCallback(() => {
+    if (page < pages) {
+      setPage(page + 1);
+    }
+  }, [page, pages]);
+
+  const onPreviousPage = React.useCallback(() => {
+    if (page > 1) {
+      setPage(page - 1);
+    }
+  }, [page]);
+
+  const onRowsPerPageChange = React.useCallback((e) => {
+    setRowsPerPage(Number(e.target.value));
+    setPage(1);
+  }, []);
+
+  const onSearchChange = React.useCallback((value) => {
+    if (value) {
+      setFilterValue(value);
+      setPage(1);
+    } else {
+      setFilterValue("");
+    }
+  }, []);
+
+  const onClear = React.useCallback(()=>{
+    setFilterValue("")
+    setPage(1)
+  },[])
+
+  const topContent = React.useMemo(() => {
+    return (
+      <div className="flex flex-col gap-4">
+        <div className="flex justify-between gap-3 items-end">
+          <Input
+            isClearable
+            className="w-full sm:max-w-[44%]"
+            placeholder="Search by Style name..."
+            startContent={<SearchIcon />}
+            value={filterValue}
+            onClear={() => onClear()}
+            onValueChange={onSearchChange}
+          />
+          <div className="flex gap-3">
+            <Dropdown>
+              <DropdownTrigger className="hidden sm:flex">
+                <Button endContent={<ChevronDownIcon className="text-small" />} variant="flat">
+                  Status
+                </Button>
+              </DropdownTrigger>
+              <DropdownMenu
+                disallowEmptySelection
+                aria-label="Table Columns"
+                closeOnSelect={false}
+                selectedKeys={statusFilter}
+                selectionMode="multiple"
+                onSelectionChange={setStatusFilter}
+              >
+                {statusOptions.map((status) => (
+                  <DropdownItem key={status.uid} className="capitalize">
+                    {capitalize(status.name)}
+                  </DropdownItem>
+                ))}
+              </DropdownMenu>
+            </Dropdown>
+            <Dropdown>
+              <DropdownTrigger className="hidden sm:flex">
+                <Button endContent={<ChevronDownIcon className="text-small" />} variant="flat">
+                  Columns
+                </Button>
+              </DropdownTrigger>
+              <DropdownMenu
+                disallowEmptySelection
+                aria-label="Table Columns"
+                closeOnSelect={false}
+                selectedKeys={visibleColumns}
+                selectionMode="multiple"
+                onSelectionChange={setVisibleColumns}
+              >
+                {columns.map((column) => (
+                  <DropdownItem key={column.uid} className="capitalize">
+                    {capitalize(column.name)}
+                  </DropdownItem>
+                ))}
+              </DropdownMenu>
+            </Dropdown>
+            <AddNewDataBtn />
+          </div>
+        </div>
+        <div className="flex justify-between items-center">
+          <span className="text-default-400 text-small">Total {reportData.length} Data</span>
+          <label className="flex items-center text-default-400 text-small">
+            Rows per page:
+            <select
+              className="bg-transparent outline-none text-default-400 text-small"
+              onChange={onRowsPerPageChange}
+            >
+              <option value="5">5</option>
+              <option value="10">10</option>
+              <option value="15">15</option>
+            </select>
+          </label>
+        </div>
+      </div>
+    );
+  }, [
+    filterValue,
+    statusFilter,
+    visibleColumns,
+    onRowsPerPageChange,
+    onSearchChange,
+    onClear,
+  ]);
+
+  const bottomContent = React.useMemo(() => {
+    return (
+      <div className="py-2 px-2 flex justify-between items-center">
+        <span className="w-[30%] text-small text-default-400">
+          {selectedKeys === "all"
+            ? "All items selected"
+            : `${selectedKeys.size} of ${filteredItems.length} selected`}
+        </span>
+        <Pagination
+          isCompact
+          showControls
+          showShadow
+          color="primary"
+          page={page}
+          total={pages}
+          onChange={setPage}
+        />
+        <div className="hidden sm:flex w-[30%] justify-end gap-2">
+          <Button isDisabled={pages === 1} size="sm" variant="flat" onPress={onPreviousPage}>
+            Previous
+          </Button>
+          <Button isDisabled={pages === 1} size="sm" variant="flat" onPress={onNextPage}>
+            Next
+          </Button>
+        </div>
+      </div>
+    );
+  }, [selectedKeys, page, onPreviousPage, onNextPage, pages, filteredItems.length]);
+
   return (
-    <div>
-      <h1>ExpenseTracker</h1>
-    </div>
-  )
+    <Table
+      aria-label="Example table with custom cells, pagination and sorting"
+      isHeaderSticky
+      bottomContent={bottomContent}
+      bottomContentPlacement="outside"
+      selectedKeys={selectedKeys}
+      selectionMode="multiple"
+      sortDescriptor={sortDescriptor}
+      topContent={topContent}
+      topContentPlacement="outside"
+      onSelectionChange={setSelectedKeys}
+      onSortChange={setSortDescriptor}
+    >
+      <TableHeader columns={headerColumns}>
+        {(column) => (
+          <TableColumn
+            key={column.uid}
+            align={column.uid === "actions" ? "center" : "start"}
+            allowsSorting={column.sortable}
+          >
+            {column.name}
+          </TableColumn>
+        )}
+      </TableHeader>
+      <TableBody emptyContent={"No Data found"} items={sortedItems}>
+        {(item) => (
+          <TableRow key={item._id}>
+            {(columnKey) => <TableCell>{renderCell(item, columnKey)}</TableCell>}
+          </TableRow>
+        )}
+      </TableBody>
+    </Table>
+  );
 }
-
-export default ExpenseTracker
